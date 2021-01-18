fe32584a0485ce14c1122e598a94a34c3fdca9e2
Brillianttyagi <deepanshubhai84@gmail.com>
2021-01-18 20:47:06 +0530

    added models

--- aipoc/aipocapi/models.py
+++ aipoc/aipocapi/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class robo(models.Model):
+    id = models.AutoField(primary_key=True)
+    status = models.BooleanField()
+    temp = models.IntegerField()
+    con_speed = models.IntegerField()
+    cpu = models.IntegerField()
+    camera = models.BooleanField()
+    ir = models.BooleanField()
+    def __str__(self):
+        return self.id
